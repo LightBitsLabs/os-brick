a4d212646329419b533c3d98ffbafa1a7cbeb405
tomerg-lb <tomer@lightbitslabs.com>
2022-01-27 11:43:36 +0200

    Fix connect volume ut
    Signed-off-by: tomerg-lb <tomer@lightbitslabs.com>

--- os_brick/tests/initiator/connectors/test_lightos.py
+++ os_brick/tests/initiator/connectors/test_lightos.py
@@ -111,8 +111,14 @@ class LightosConnectorTestCase(test_connector.ConnectorTestCase):
 
     @mock.patch.object(lightos.LightOSConnector, 'get_hostnqn',
                        return_value=FAKE_NQN)
-    def test_dsc_do_connect_volume_succeed(self, mock_nqn):
-        self.connector.dsc_do_connect_volume(self._get_connection_info())
+    @mock.patch.object(lightos.priv_lightos, 'move_dsc_file',
+                       return_value="/etc/discovery_client/discovery.d/v0")
+    @mock.patch.object(lightos.LightOSConnector,
+                       '_check_device_exists_using_dev_lnk',
+                       return_value="/dev/nvme0n1")
+    def test_connect_volume_succeed(self, mock_nqn, mock_move_file,
+                                    mock_check_device):
+        self.connector.connect_volume(self._get_connection_info())
 
     @mock.patch.object(lightos.LightOSConnector, 'get_hostnqn',
                        return_value=FAKE_NQN)
