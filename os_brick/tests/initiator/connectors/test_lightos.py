6d0ba0736d1ad5738a6ba59053d5a19800d38885
tomerg-lb <tomer@lightbitslabs.com>
2022-01-27 12:20:43 +0200

    Fix connect volume failure ut
    Signed-off-by: tomerg-lb <tomer@lightbitslabs.com>

--- os_brick/tests/initiator/connectors/test_lightos.py
+++ os_brick/tests/initiator/connectors/test_lightos.py
@@ -20,6 +20,7 @@ from unittest.mock import mock_open
 
 from oslo_concurrency import processutils as putils
 
+from os_brick import exception
 from os_brick.initiator.connectors import lightos
 from os_brick.privileged import lightos as priv_lightos
 from os_brick.tests.initiator import test_connector
@@ -122,11 +123,16 @@ class LightosConnectorTestCase(test_connector.ConnectorTestCase):
 
     @mock.patch.object(lightos.LightOSConnector, 'get_hostnqn',
                        return_value=FAKE_NQN)
-    @mock.patch.object(lightos.LightOSConnector, '_execute',
-                       side_effect=putils.ProcessExecutionError)
-    def test_dsc_do_connect_volume_failure(self, mock_nqn, execute_mock):
-        self.assertRaises(putils.ProcessExecutionError,
-                          self.connector.dsc_do_connect_volume,
+    @mock.patch.object(lightos.priv_lightos, 'move_dsc_file',
+                       return_value="/etc/discovery_client/discovery.d/v0")
+    @mock.patch.object(lightos.priv_lightos, 'delete_dsc_file',
+                       return_value=None)
+    @mock.patch.object(lightos.LightOSConnector, '_get_device_by_uuid',
+                       return_value=None)
+    def test_connect_volume_failure(self, mock_nqn, mock_move_file,
+                                    mock_delete_file, mock_get_device):
+        self.assertRaises(exception.BrickException,
+                          self.connector.connect_volume,
                           self._get_connection_info())
 
     @mock.patch.object(priv_lightos, 'delete_dsc_file', return_value=True)
